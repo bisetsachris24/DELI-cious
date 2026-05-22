785f9f7ab94b81184784c2e91731cc23e2e1300a
amanichristianbisetsa <bisetsachristian@gmail.com>
2026-05-22 09:36:31 -0500

    I created Sandwich class with bread type, size, and toasted properties

--- src/main/java/com/pluralsight/models/Sandwich.java
+++ src/main/java/com/pluralsight/models/Sandwich.java
@@ -1,4 +1,20 @@
 package com.pluralsight.models;
 
 public class Sandwich {
+
+    // I am creating prperties for sandwich that handles bread type, size and toppings
+    // i
+    private String breadType;
+    private int size;
+    private boolean toasted;
+
+    // Constructor
+    public Sandwich(String breadType, int size, boolean toasted) {
+        this.breadType = breadType;
+        this.size = size;
+        this.toasted = toasted;
+    }
+
+
+
 }

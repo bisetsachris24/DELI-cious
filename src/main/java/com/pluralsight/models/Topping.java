ae1744ac543155ee7fef7e1d95bb458a13380d43
amanichristianbisetsa <bisetsachristian@gmail.com>
2026-05-22 10:39:27 -0500

    Add Topping class with topping types and extra topping support

--- src/main/java/com/pluralsight/models/Topping.java
+++ src/main/java/com/pluralsight/models/Topping.java
@@ -1,4 +1,24 @@
 package com.pluralsight.models;
 
 public class Topping {
+    public enum ToppingType {
+        MEAT, CHEESE, REGULAR, SAUCE, SIDE
+    }
+
+    private String name;
+    private ToppingType type;
+    private boolean isExtra;
+
+    public Topping(String name, ToppingType type) {
+        this.name = name;
+        this.type = type;
+        this.isExtra = false;
+    }
+
+    public Topping(String name, ToppingType type, boolean isExtra) {
+        this.name = name;
+        this.type = type;
+        this.isExtra = isExtra;
+    }
+
 }

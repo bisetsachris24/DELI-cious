d719f9a706e8bfc3c1061ae5550e46723c471885
amanichristianbisetsa <bisetsachristian@gmail.com>
2026-05-22 10:44:25 -0500

    Add Drink class with size and flavor properties Create Size enum with SMALL, MEDIUM, and LARGE options Implement constructor for drink initialization

--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -1,4 +1,17 @@
 package com.pluralsight.models;
 
 public class Drink {
+
+    public enum Size {
+        SMALL, MEDIUM, LARGE
+    }
+
+    private Size size;
+    private String flavor;
+
+    public Drink(Size size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
 }

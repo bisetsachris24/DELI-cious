7a7222d3d7d9648a51ba989d4dca832bc09cd8b8
amanichristianbisetsa <bisetsachristian@gmail.com>
2026-05-22 13:03:15 -0500

    Add Chips class for order side items Implement chip type property and fixed pricing Create constructor and price getter method

--- src/main/java/com/pluralsight/models/Chips.java
+++ src/main/java/com/pluralsight/models/Chips.java
@@ -1,4 +1,18 @@
 package com.pluralsight.models;
 
+// Represents a bag of chips added to an order.
+
 public class Chips {
+
+    private static final double PRICE = 1.50;
+
+    private String chipType;
+
+    public Chips(String chipType) {
+        this.chipType = chipType;
+    }
+
+    public double getPrice() {
+        return PRICE;
+    }
 }

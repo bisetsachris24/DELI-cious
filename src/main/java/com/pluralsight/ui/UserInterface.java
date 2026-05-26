5b2e40191b19068d1ebfe19ec867eab34275e086
amanichristianbisetsa <bisetsachristian@gmail.com>
2026-05-26 13:41:23 -0500

    Set up UserInterface menu options and input handling Added Scanner support for reading customer input from the console. Created predefined menu option arrays for breads, meats, cheeses, toppings, sauces, sides, and chips. Initialized UserInterface constructor for interactive order processing.

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -1,4 +1,27 @@
 package com.pluralsight.ui;
 
+import java.util.Scanner;
+
 public class UserInterface {
+    // Scanner object used to read user input from the console.
+    private Scanner scanner;
+
+
+    // Available options (for display)
+// Available bread choices for sandwiches.
+    private static final String[] BREAD_TYPES = {"white", "wheat", "rye", "wrap"};
+    private static final String[] MEATS = {"steak", "ham", "salami", "roast beef", "chicken", "bacon"};
+    private static final String[] CHEESES = {"american", "provolone", "cheddar", "swiss"};
+    private static final String[] TOPPINGS = {"lettuce", "peppers", "onions", "tomatoes",
+            "jalapeños", "cucumbers", "pickles", "guacamole", "mushrooms"};
+    private static final String[] SAUCES = {"mayo", "mustard", "ketchup", "ranch",
+            "thousand islands", "vinaigrette"};
+    private static final String[] SIDES = {"au jus", "sauce"};
+    private static final String[] CHIP_TYPES = {"Classic", "BBQ", "Sour Cream & Onion",
+            "Salt & Vinegar", "Jalapeño"};
+
+    //Creates a Scanner object for reading console input.
+    public UserInterface() {
+        this.scanner = new Scanner(System.in);
+    }
 }

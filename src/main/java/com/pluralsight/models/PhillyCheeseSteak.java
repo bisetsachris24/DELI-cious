3fe6d82b49dbf560acc15e04eb81a8516b87dbdc
amanichristianbisetsa <bisetsachristian@gmail.com>
2026-05-28 11:08:40 -0500

    Add PhillyCheeseSteak signature sandwich with default toppings

--- src/main/java/com/pluralsight/models/PhillyCheeseSteak.java
+++ src/main/java/com/pluralsight/models/PhillyCheeseSteak.java
@@ -1,4 +1,17 @@
 package com.pluralsight.models;
 
-public class PhillyCheeseSteak {
+public class PhillyCheeseSteak extends Sandwich {
+
+    public static final String SIGNATURE_NAME = "Philly Cheese Steak";
+
+    public PhillyCheeseSteak() {
+        // 8-inch white bread, toasted
+        super("white", 8, true);
+
+        // Pre-load the Philly recipe
+        addMeat("steak", false);
+        addCheese("american", false);
+        addTopping("peppers");
+        addSauce("mayo");
+    }
 }
